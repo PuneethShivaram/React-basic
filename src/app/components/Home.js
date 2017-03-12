bf7d6b717948d88f8966852185dce11c312a22c9
PuneethShivaram <puneet.shivram@gmail.com>
2017-03-12 13:16:32 +0530

    added files

---
+++ src/app/components/Home.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default class Home extends React.Component{
+  render(){
+    console.log(this.props)
+    return (
+      <p>This is the body content</p>
+    );
+  }
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,21 @@
-console.log("Hey it's working well!");
+import React from "react";
+import Home from "./components/Home"
+import Header from "./components/Header"
+import {render} from "react-dom";
+
+class App extends React.Component {
+  render(){
+    const user = {
+      name:"puneeth",
+      hobbies: ["Eating"]
+    }
+    return (
+      <div>
+          <Header/>
+          <Home name={"puneeth"} age={27} user={user}/>
+      </div>
+    )
+  }
+}
+
+render(<App/>,window.document.getElementById('app'))
